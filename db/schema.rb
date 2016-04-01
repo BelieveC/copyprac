f77ba50c70f1cbfe5b5f4d3046ffeac4ee6d4085
Chaitanya Yadav <cayadav2009@gmail.com>
2016-04-02 04:37:39 +0530

    Image model Added

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160401204523) do
+ActiveRecord::Schema.define(version: 20160401225456) do
+
+  create_table "images", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "practicals", force: :cascade do |t|
     t.string   "aim"
