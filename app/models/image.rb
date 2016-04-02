69859b817d2e6f33bcbf71a30b4de622bf1079f0
Chaitanya Yadav <cayadav2009@gmail.com>
2016-04-02 12:50:34 +0530

    Nested Image Upload Done

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,7 +1,7 @@
 class Image < ActiveRecord::Base
 	belongs_to :practical
 
-  validates :sheet,:title,:description, presence: true
+  validates :sheet, presence: true
   has_attached_file :sheet, styles: { medium: "400*400#"}
   validates_attachment_content_type :sheet, content_type: /\Aimage\/.*\Z/
 end
