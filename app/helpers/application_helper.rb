98f9649d2dca5bca8bb1c3ad13b041580cb2b2e1
Chaitanya Yadav <cayadav2009@gmail.com>
2016-04-03 05:23:29 +0530

    Show page configured

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,22 @@
 module ApplicationHelper
+	def current_user
+		@current_user ||= User.find(session[:user_id]) if session[:user_id]
+	end
+
+	def authenticate_user
+		if session[:user_id]
+			@current_user ||=User.find(session[:user_id])
+			redirect_to :back
+		else
+			redirect_to "#",notice:"You have to Log In to Perform this Action!"
+		end
+	end
+
+	def user_signed_in?
+		if session[:user_id]
+			return true
+		else
+			false
+		end
+	end
 end
