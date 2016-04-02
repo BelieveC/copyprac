bc8198be4dc80d095e57d1f2f2c2dcf656c13b19
Chaitanya Yadav <cayadav2009@gmail.com>
2016-04-02 06:26:50 +0530

    Routes and Controller Updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
 
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/failure', to: redirect('/')
+  get 'signout', to: 'sessions#destroy', as: 'signout'
+
+  resources :sessions, only: [:create, :destroy]
   root 'practicals#about'
 
   resources :practicals
