b00e0072ab01cd0a0c6348ef887341f36d9f0b59
Chaitanya Yadav <cayadav2009@gmail.com>
2016-04-02 15:12:09 +0530

    Examnote controller updated

--- app/controllers/examnotes_controller.rb
+++ app/controllers/examnotes_controller.rb
@@ -1,2 +1,50 @@
 class ExamnotesController < ApplicationController
+	before_action :get_examnote, only:[:show,:edit,:update,:destroy]
+	def about
+	end
+
+	def index
+		@examnotes = Examnote.all.order("created_at DESC")
+	end
+
+	def show
+	end
+
+	def new
+		@examnote = Examnote.new
+	end
+
+	def create
+		@examnote = Examnote.new(examnote_params)
+		if @examnote.save
+			redirect_to @examnote
+		else
+			render('new')
+		end
+	end
+
+	def edit
+	end
+
+	def update
+		if @examnote.update(examnote_params)
+			redirect_to @examnote
+		else
+			render('edit')
+		end
+	end
+
+	def destroy
+		@examnote.destroy
+		redirect_to examnotes_path
+	end
+
+	private
+		def examnote_params
+			params.require(:examnote).permit(:subject,:topic,:tips)
+		end
+
+		def get_pratical
+			@examnote = Examnote.find(params[:id])
+		end
 end
