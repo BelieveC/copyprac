1d87a4d938a17ba3e9ee536b459798e0a1ed9ab2
Chaitanya Yadav <cayadav2009@gmail.com>
2016-04-02 06:17:27 +0530

    User model Created

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160401235145) do
+ActiveRecord::Schema.define(version: 20160402004606) do
 
   create_table "images", force: :cascade do |t|
     t.datetime "created_at",         null: false
@@ -28,6 +28,16 @@ ActiveRecord::Schema.define(version: 20160401235145) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "name"
+    t.string   "oauth_token"
+    t.datetime "oauth_expires_at"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
   end
 
 end
