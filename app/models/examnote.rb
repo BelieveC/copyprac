c5821bf14c4c4571b6f1a28e892d3e9714da9073
Chaitanya Yadav <cayadav2009@gmail.com>
2016-04-02 23:21:00 +0530

    Models updated

--- app/models/examnote.rb
+++ app/models/examnote.rb
@@ -1,3 +1,5 @@
 class Examnote < ActiveRecord::Base
 	has_many :notes
+	accepts_nested_attributes_for :notes, reject_if: :all_blank, allow_destroy: true
+
 end
--- app/models/note.rb
+++ app/models/note.rb
@@ -1,3 +1,6 @@
 class Note < ActiveRecord::Base
 	belongs_to :examnote
+	 validates :page, presence: true
+	  has_attached_file :page, styles: { medium: "400*400#"}
+	  validates_attachment_content_type :page, content_type: /\Aimage\/.*\Z/
 end
