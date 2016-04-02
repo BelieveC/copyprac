b1795e8e8a4f4f0591b260236c3ec4589bc37d9c
Chaitanya Yadav <cayadav2009@gmail.com>
2016-04-02 06:29:25 +0530

    Session Controller Updated

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,12 @@
 class SessionsController < ApplicationController
+	def create
+	    user = User.from_omniauth(env["omniauth.auth"])
+	    session[:user_id] = user.id
+	    redirect_to root_path,notice:"Successfully Signed In"
+	end
+
+	def destroy
+		session[:user_id] = nil
+	    redirect_to root_path,notice:"Logged Out"
+	end
 end
