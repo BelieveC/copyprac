39ea1778f158916026291d534365d016a8b24a94
Chaitanya Yadav <cayadav2009@gmail.com>
2016-04-02 14:43:01 +0530

    Image Url problem solved

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,8 +11,17 @@ class ApplicationController < ActionController::Base
  def authenticate_user
  	if session[:user_id]
  		@current_user ||=User.find(session[:user_id])
+ 		redirect_to :back
  	else
  		redirect_to "#",notice:"You have to Log In to Perform this Action!"
+ 	end
+ end
+
+ def user_signed_in?
+ 	if session[:user_id]
+ 		return true
+ 	else
+ 		false
  	end
  end
 end
