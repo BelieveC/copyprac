0f63b5d4a527803ab1b77922d5e351f5cf1abdc9
Chaitanya Yadav <cayadav2009@gmail.com>
2016-04-05 20:22:45 +0530

    College controller updated

--- app/controllers/colleges_controller.rb
+++ app/controllers/colleges_controller.rb
@@ -1,7 +1,48 @@
 class CollegesController < ApplicationController
-  def name:string
-  end
 
-  def address:string
-  end
+	def new
+		@college = College.new
+	end
+
+	def index
+		@colleges = College.all.order("name ASC")
+	end
+
+	def show
+	end
+
+	def create
+		@college = College.new(college_params)
+		if @college.save
+			redirect_to colleges_path
+		else
+			render('new')
+		end
+	end
+
+	def edit
+	end
+
+	def update
+		if @college.update(college_params)
+			redirect_to colleges_path
+		else
+			render('edit')
+		end
+	end
+
+	def destroy
+		@college.destroy
+		redirect_to colleges_path
+	end
+
+	private
+
+		def college_params
+			params.require(:college).permit(:name,:address)
+		end
+
+		def get_college
+			@college = College.find[params[:id]]
+		end
 end
