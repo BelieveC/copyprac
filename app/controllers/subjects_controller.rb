1a6127c2d6e97817bdd232ed54c4faf4b4831004
Chaitanya Yadav <cayadav2009@gmail.com>
2016-04-05 22:44:43 +0530

    Subject added

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,18 +1,19 @@
 class SubjectsController < ApplicationController
+	before_action :get_subject, only:[:show,:edit,:update,:destroy]
 	def new
-		subject = Subject.new
+		@subject = Subject.new
 	end
 
 	def index
-		subjects = Subject.all.order("name ASC")
+		@subjects = Subject.all.order("name ASC")
 	end
 
 	def show
 	end
 
 	def create
-		subject = Subject.new(subject_params)
-		if subject.save
+		@subject = Subject.new(subject_params)
+		if @subject.save
 			redirect_to subjects_path
 		else
 			render('new')
@@ -23,7 +24,7 @@ class SubjectsController < ApplicationController
 	end
 
 	def update
-		if subject.update(subject_params)
+		if @subject.update(subject_params)
 			redirect_to subjects_path
 		else
 			render('edit')
@@ -31,18 +32,18 @@ class SubjectsController < ApplicationController
 	end
 
 	def destroy
-		subject.destroy
+		@subject.destroy
 		redirect_to subjects_path
 	end
 
 	private
 
 		def subject_params
-			params.require(:college).permit(:name)
+			params.require(:subject).permit(:name)
 		end
 
 		def get_subject
-			subject = Subject.find[params[:id]]
+			@subject = Subject.find(params[:id])
 		end
 
 end
