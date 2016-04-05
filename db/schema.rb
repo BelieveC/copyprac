415a0914370b0a57ff84c41f75d361296f0e0701
Chaitanya Yadav <cayadav2009@gmail.com>
2016-04-05 20:01:16 +0530

    Subject Model Added

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160405142030) do
+ActiveRecord::Schema.define(version: 20160405142832) do
 
   create_table "colleges", force: :cascade do |t|
     t.string   "name"
@@ -76,6 +76,12 @@ ActiveRecord::Schema.define(version: 20160405142030) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "subjects", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "tcomments", force: :cascade do |t|
