4a79f2b836d9cc7b4f04847ee3bc1fe04db46861
Chaitanya Yadav <cayadav2009@gmail.com>
2016-04-05 18:58:48 +0530

    Controller updated for comments;

--- app/controllers/ecomments_controller.rb
+++ app/controllers/ecomments_controller.rb
@@ -1,2 +1,23 @@
 class EcommentsController < ApplicationController
+	before_action :get_examnote
+	def create
+		@ecomment = @examnote.ecomments.create(ecomment_params)
+		redirect_to @examnote
+	end
+
+	def destroy
+		@ecomment = @examnote.ecomments.find(params[:id])
+		@ecomment.destroy
+		redirect_to @examnote
+	end
+
+	private
+		def get_examnote
+			@examnote = Examnote.find(params[:examnote_id])
+		end
+
+		def ecomment_params
+			params.require(:ecomment).permit(:examnote_id,:body)
+		end
+
 end
--- app/controllers/tcomments_controller.rb
+++ app/controllers/tcomments_controller.rb
@@ -1,2 +1,23 @@
 class TcommentsController < ApplicationController
+	before__action :get_thesis
+	def create
+		@thesis.tcomments.create(tcomment_params)
+		redirect_to thesiss_path(@thesis)
+	end
+
+	def destroy
+		@tcomment = @thesis.tcomments.find(params[:id])
+		@tcomment.destroy
+	end
+
+	private
+
+		def get_thesis
+			@thesis = Thesis.find(params[:thesis_id])
+		end
+
+		def tcomments_params
+			params.require(:tcomment).permit(:thesis_id,:body)
+		end
 end
+
