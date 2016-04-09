e96580bd5ae5767acef2566ca7fc7ec787c06c17
Chaitanya Yadav <cayadav2009@gmail.com>
2016-04-09 12:07:44 +0530

    Assignment controller Configured

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -1,2 +1,47 @@
 class AssignmentsController < ApplicationController
+	before_action :get_assignment,only: [:show,:edit,:update,:destroy]
+	def index
+		@assignments = Assignment.all.order("created_at ASC")
+	end
+
+	def show
+	end
+
+	def edit
+	end
+
+	def update
+		if @assignment.update(assignment_params)
+			redirect_to @assignment
+		else
+			render('edit')]
+		end
+	end
+
+	def new
+		@assignment = Assignment.new
+	end
+
+	def create
+		@assignment = Assignment.new(assignment_params)
+		if @assignment.save
+			redirect_to @assignment
+		else
+			render('new')
+		end
+	end
+
+	def destroy
+		@assignment.destroy
+		redirect_to assignments_path
+	end
+
+	private
+		def assignment_params
+			params.require(:assignment).permit(:topic,:subject_id,:college_id,:description,:teacher)
+		end
+
+		def get_assignment
+			@assignment = Assignment.find(params[:id])
+		end
 end
