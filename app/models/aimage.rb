da18f1fc7a481d99a6c1c98c83e44d48061fc5b8
Chaitanya Yadav <cayadav2009@gmail.com>
2016-04-10 12:31:10 +0530

    Relationship betn Aimages and Assignment added

--- app/models/aimage.rb
+++ app/models/aimage.rb
@@ -1,5 +1,5 @@
 class Aimage < ActiveRecord::Base
-	has_many :assignments
+	belongs_to :assignment
 	validates :image, presence: true
   	has_attached_file :image, styles: { medium: "400*400#"}
   	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,2 +1,4 @@
 class Assignment < ActiveRecord::Base
+	has_many :aimages
+	accepts_nested_attributes_for :aimages, reject_if: :all_blank, allow_destroy: true
 end
