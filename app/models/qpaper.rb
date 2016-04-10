df48db37a665d0629e7532d09e7c4479e6d97ac0
Chaitanya Yadav <cayadav2009@gmail.com>
2016-04-10 14:38:51 +0530

    Model bug fixed

--- app/models/qpaper.rb
+++ app/models/qpaper.rb
@@ -1,5 +1,5 @@
 class Qpaper < ActiveRecord::Base
 	searchkick
 	has_many :qimages
-	accepts_nested_attributes_for :images, reject_if: :all_blank, allow_destroy: true
+	accepts_nested_attributes_for :qimages, reject_if: :all_blank, allow_destroy: true
 end
