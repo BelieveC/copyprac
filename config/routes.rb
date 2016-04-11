d632c0e7bc87dae6bc77e81d86ba344d964b8d5d
Chaitanya Yadav <cayadav2009@gmail.com>
2016-04-11 12:33:13 +0530

    Request  resource added

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :colleges
   resources :qpapers
   resources :assignments
-
+  resources :requests
   get 'auth/:provider/callback', to: 'sessions#create'
   get 'auth/failure', to: redirect('/')
   get 'signout', to: 'sessions#destroy', as: 'signout'
