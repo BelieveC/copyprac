133c6cb9cbedc448f68cbe599056183b03b0bea1
Chaitanya Yadav <cayadav2009@gmail.com>
2016-04-11 12:42:36 +0530

    COntroller Request updated

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,2 +1,45 @@
 class RequestsController < ApplicationController
+	before :get_request, only:[:show,:edit,:update,:destroy]
+	def index
+		if params[:subject].blank?
+			@requests = Request.all.order("created_at DESC")
+		else
+			@requests = Request.where(subject_id: params[:subject]).order("created_at DESC")
+		end
+	end
+	def show
+	end
+	def new
+		@request = Request.new
+	end
+	def update
+		if @request.update(request_params)
+			redirect_to @request
+		else
+			render('edit')
+		end
+	end
+	def edit
+	end
+	def create
+		@request = Request.new(request_params)
+		if @request.save
+			redirect_to @request
+		else
+			render 'new'
+		end
+	end
+	def destroy
+		@request.destroy
+		redirect_to requests_path
+	end
+
+	private
+		def get_request
+			@request = Request.find(params[:id])
+		end
+
+		def request_params
+			params.require(:request).permit(:name,:type,:subject_id)
+		end
 end
