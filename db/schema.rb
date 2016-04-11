8c5d1e43baf4b4d716add12ae274a44d0aeeeb45
Chaitanya Yadav <cayadav2009@gmail.com>
2016-04-11 13:15:47 +0530

    Type column removed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160411073756) do
+ActiveRecord::Schema.define(version: 20160411074112) do
 
   create_table "aimages", force: :cascade do |t|
     t.integer  "assignment_id"
@@ -129,7 +129,6 @@ ActiveRecord::Schema.define(version: 20160411073756) do
 
   create_table "requests", force: :cascade do |t|
     t.string   "name"
-    t.string   "type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "subject_id"
