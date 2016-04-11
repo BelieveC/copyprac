9c19756e6306a83c016958d2ab18dbde4e5acc37
Chaitanya Yadav <cayadav2009@gmail.com>
2016-04-11 13:35:25 +0530

    Description added to Requests model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160411074112) do
+ActiveRecord::Schema.define(version: 20160411075851) do
 
   create_table "aimages", force: :cascade do |t|
     t.integer  "assignment_id"
@@ -129,10 +129,11 @@ ActiveRecord::Schema.define(version: 20160411074112) do
 
   create_table "requests", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.integer  "subject_id"
     t.integer  "type_id"
+    t.text     "description"
   end
 
   create_table "subjects", force: :cascade do |t|
