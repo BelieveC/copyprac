8072be46060361c69445586775f9410ddbb5e8d0
Chaitanya Yadav <cayadav2009@gmail.com>
2016-04-23 15:05:19 +0530

    Omniauth removed

--- config/initializers/omniauth.rb
+++
@@ -1,5 +0,0 @@
-OmniAuth.config.logger = Rails.logger
-
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '', '', {:client_options => {:ssl => {:ca_file => Rails.root.join("cacert.pem").to_s}}}
-end
